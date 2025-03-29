2d3faf85af39f90f3f4339d2d2ed4cd4c841d8f0
octogradiste <colin.berger@outlook.com>
2025-03-29 19:47:59 +0100

    Disable lazy package update notifications

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -24,5 +24,5 @@ require("lazy").setup({
 		{ import = "plugins" },
 	},
 	install = { colorscheme = { "tokyonight" } },
-	checker = { enabled = true },
+	checker = { enabled = true, notify = false },
 })
