2e4efbdac70010c6b53c6674d4318d35a2f52546
octogradiste <colin.berger@outlook.com>
2025-06-17 18:58:00 +0200

    Remove ensure installed list

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,33 +6,6 @@ return {
 		local opts = {
 			highlight = { enable = true },
 			indent = { enable = true },
-			ensure_installed = {
-				"asm",
-				"bash",
-				"c",
-				"cmake",
-				"cpp",
-				"css",
-				"csv",
-				"dart",
-				"diff",
-				"dockerfile",
-				"gdscript",
-				"go",
-				"html",
-				"java",
-				"javascript",
-				"json",
-				"kotlin",
-				"lua",
-				"make",
-				"nix",
-				"python",
-				"rust",
-				"tsx",
-				"typescript",
-				"yaml",
-			},
 		}
 
 		require("nvim-treesitter.configs").setup(opts)
