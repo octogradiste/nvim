d17cc1dbd080133fb12d20f4131a9e5f6dd29e2f
octogradiste <colin.berger@outlook.com>
2025-09-18 09:03:04 +0200

    Fix vim global warnings + clean up autotag config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -4,7 +4,19 @@ return {
 		local capabilities = require("cmp_nvim_lsp").default_capabilities()
 		local lspconfig = require("lspconfig")
 
-		lspconfig.lua_ls.setup({ capabilities = capabilities })
+		lspconfig.lua_ls.setup({
+			capabilities = capabilities,
+			settings = {
+				Lua = {
+					diagnostics = {
+						globals = { "vim" },
+					},
+					workspace = {
+						library = vim.api.nvim_get_runtime_file("", true),
+					},
+				},
+			},
+		})
 		lspconfig.nixd.setup({ capabilities = capabilities })
 		lspconfig.openscad_lsp.setup({ capabilities = capabilities })
 		lspconfig.pyright.setup({ capabilities = capabilities })
