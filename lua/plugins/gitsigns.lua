680821180a4cdb291ca6bd4e64bf6ffa8e6eb086
octogradiste <colin.berger@outlook.com>
2025-09-18 11:33:29 +0200

    Prefix git commands with g

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -8,12 +8,12 @@ return {
 				vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
 			end
 
-			map("n", "<leader>hs", gs.stage_hunk, "Gitsigns stage hunk")
-			map("n", "<leader>hr", gs.reset_hunk, "Gitsigns reset hunk")
-			map("n", "<leader>bs", gs.stage_buffer, "Gitsigns stage buffer")
-			map("n", "<leader>br", gs.reset_buffer, "Gitsigns reset buffer")
-			map("n", "<leader>dt", gs.diffthis, "Gitsigns diff this")
-			map("n", "<leader>tb", gs.toggle_current_line_blame, "Gitsigns toggle blame")
+			map("n", "<leader>gsh", gs.stage_hunk, "Gitsigns stage hunk")
+			map("n", "<leader>grh", gs.reset_hunk, "Gitsigns reset hunk")
+			map("n", "<leader>gsb", gs.stage_buffer, "Gitsigns stage buffer")
+			map("n", "<leader>grb", gs.reset_buffer, "Gitsigns reset buffer")
+			map("n", "<leader>gdt", gs.diffthis, "Gitsigns diff this")
+			map("n", "<leader>gtb", gs.toggle_current_line_blame, "Gitsigns toggle blame")
 		end,
 	},
 }
