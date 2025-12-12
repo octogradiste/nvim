ff0ef6748f2efcd6b2d07b82be533e387571f7e6
octogradiste <colin.berger@outlook.com>
2025-12-12 16:12:23 +0100

    Add key map to toggle copilot

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,5 +1,8 @@
 return {
 	"zbirenbaum/copilot.lua",
+	keys = {
+		{ "<leader>co", ":Copilot toggle<cr>", desc = "Toggle Copilot" },
+	},
 	opts = {
 		suggestion = { enabled = false },
 		panel = { enabled = false },
